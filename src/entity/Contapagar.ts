ade347f348b2d13672225326f3d39b769b2981d6
GuiiiOberderfer <108491508+GuiiiOberderfer@users.noreply.github.com>
2024-06-27 10:52:04 -0300

    Contapagar.ts

---
+++ src/entity/Contapagar.ts
@@ -0,0 +1,18 @@
+import { Entity, Column } from "typeorm";
+import { GenericEntity } from "./GenericEntity";
+
+@Entity()
+export class ContaPagar extends GenericEntity {
+
+    @Column({default: '', nullable: true})
+    fornecedorId: number;
+
+    @Column({type: 'decimal', nullable: true})
+    valor: number;
+
+    @Column({type: 'date', nullable: true})
+    dataVencimento: Date;
+
+    @Column({default: '', nullable: true})
+    observacoes: string;
+}
