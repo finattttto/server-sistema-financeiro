b03803c50f6fdf60272acefa6c68b56dc2e7c679
FernandoLuisLoff <fernandoloff123@gmail.com>
2024-07-02 22:22:42 -0300

    Criados as rotas das controllers

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,65 @@
 import { Router } from "express";
+import ClienteController from "../controller/ClienteController";
+import ContapagarController from "../controller/ContapagarController";
+import ContareceberController from "../controller/ContareceberController";
+import ProdutoController from "../controller/ProdutoController";
+import UsuarioController from "../controller/UsuarioController";
+import VendaController from "../controller/VendaController";
 
 const routes = Router();
 
-// routes.route('/relatorios')
-// 		.get(RelatorioController.index)
-// 		.post(RelatorioController.save);
+routes.route('/clientes')
+		.get(ClienteController.index)
+		.post(ClienteController.save);
 
-// routes.route('/relatorios/:id')
-// 		.get(RelatorioController.show)
-// 		.put(RelatorioController.update)
-// 		.delete(RelatorioController.remove);
+routes.route('/clientes/:id')
+		.get(ClienteController.show)
+		.put(ClienteController.update)
+		.delete(ClienteController.remove);
 
+routes.route('/contapagar')
+		.get(ContapagarController.index)
+		.post(ContapagarController.save);
+
+routes.route('/contapagar/:id')
+		.get(ContapagarController.show)
+		.put(ContapagarController.update)
+		.delete(ContapagarController.remove);
+
+routes.route('/contareceber')
+        .get(ContareceberController.index)
+        .post(ContareceberController.save);
+
+routes.route('/contareceber/:id')
+        .get(ContareceberController.show)
+        .put(ContareceberController.update)
+        .delete(ContareceberController.remove);
+
+routes.route('/produtos')
+        .get(ProdutoController.index)
+        .post(ProdutoController.save);
+
+routes.route('/produtos/:id')
+        .get(ProdutoController.show)
+        .put(ProdutoController.update)
+        .delete(ProdutoController.remove);
+
+routes.route('/usuarios')
+        .get(UsuarioController.index)
+        .post(UsuarioController.save);
+
+routes.route('/usuarios/:id')
+        .get(UsuarioController.show)
+        .put(UsuarioController.update)
+        .delete(UsuarioController.remove);
+
+routes.route('/venda')
+        .get(VendaController.index)
+        .post(VendaController.save);
+
+routes.route('/venda/:id')
+        .get(VendaController.show)
+        .put(VendaController.update)
+        .delete(VendaController.remove);
+        
 export default routes;
